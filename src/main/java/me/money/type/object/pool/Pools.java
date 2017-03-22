1e23295b1f84f39440ddd0f260403d2756e8c26a
niucqing@126.com <niucqing@126.com>
2017-03-23 00:33:08 +0800

    pools update

--- src/main/java/me/money/type/object/pool/Pools.java
+++ src/main/java/me/money/type/object/pool/Pools.java
@@ -5,8 +5,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ * @author Niucqing
+ * @email niucqing@gmail.com
+ */
 public class Pools {
 	private static final ExecutorService es = Executors.newFixedThreadPool(10);
+//	private static final ExecutorService ess = Executors.newCachedThreadPool();
 
 	/**
 	 * submit 有返回值，如果过程有异常，可以通过future.get()重新捕获异常
@@ -28,7 +33,19 @@ public class Pools {
 		return es.submit(callable);
 	}
 
+	/**
+	 * 等待所有任务执行完毕之后执行shutdown
+	 * 建议使用
+	 */
 	public static void shutdown() {
 		es.shutdown();
+	}
+
+	/**
+	 * 正在执行中的任务也会停止
+	 * 不建议使用
+	 */
+	public static void shutdownNow() {
+		es.shutdownNow();
 	}
 }
